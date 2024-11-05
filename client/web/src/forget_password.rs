3fce0753b2c7d4ce90163887550d7f11bab45775
Obei Sideg <obei.sideg@gmail.com>
2024-11-05 20:02:23 +0300

    web: Add forget password page

--- client/web/src/forget_password.rs
+++ client/web/src/forget_password.rs
@@ -1,11 +1,56 @@
 use dioxus::prelude::*;
 
+use crate::index::Route;
+
 #[component]
 pub fn ForgetPassword() -> Element {
     rsx! {
         div {
             class: "forget-password",
-            div { "Forget Password" }
+            div {
+                class: "forget-password__header",
+                div {
+                    class: "forget-password__header--logo",
+                    Link {
+                        to: Route::Index {},
+                        div { "Mubdel" }
+                    }
+                }
+                div {
+                    class: "forget-password__header--links",
+                    Link {
+                        to: Route::Login {},
+                        div { "Login" }
+                    }
+                    Link {
+                        to: Route::Signup {},
+                        div { "Sign up" }
+                    }
+                }
+            }
+            div {
+                class: "forget-password__box",
+                div {
+                    class: "forget-password-box",
+                    h4 { "Request Password Reset" }
+                    div {
+                        class: "forget-password-box__email",
+                        label { "Email" }
+                        input {}
+                    }
+                    div {
+                        class: "forget-password-box__actions",
+                        Link {
+                            to: Route::Login {},
+                            div { "Cancel" }
+                        }
+                        button {
+                            class: "btn-primary",
+                            "Submit"
+                        }
+                    }
+                }
+            }
         }
     }
 }
